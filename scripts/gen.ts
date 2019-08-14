b14b056ec07f6101255a09e6b7571faa632f9efb
Luke <lukeautry@gmail.com>
2019-08-14 17:20:07 -0400

    about page updates

--- scripts/gen.ts
+++ scripts/gen.ts
@@ -52,11 +52,6 @@ export const generate = () => {
       <link href="https://fonts.googleapis.com/css?family=Gothic+A1:200|Ubuntu+Mono" rel="stylesheet">
 
       <link rel="stylesheet" href="/${computedPath}/index.css" type="text/css">
-
-      <script type="text/javascript">
-          window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
-            heap.load("129285141");
-      </script>
     </head>
 
     <body>
